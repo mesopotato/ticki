2ab784564fb8a7afcb2da5a91f833337ec1ada73
mesopotato <dittrich.yannick@gmail.com>
2019-04-21 22:14:24 +0200

    bild und eventcreation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,6 +13,21 @@ var nodemailer = require('nodemailer');
 var Event = require('../models/event');
 var Tickets = require('../models/tickets');
 var appNpay = require('../models/appNpayment');
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: 'dzcxfnvyu',
+    api_key: '771478566264235',
+    api_secret: '9VxATR7G6tjNLkqmEDGE5rQUoV8'
+});
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "demo",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{ width: 80, height: 80, crop: "limit" }]
+});
+const parser = multer({ storage: storage });
 
 router.get('/newtickets', function (req, res) {
     res.render('newtickets', {
@@ -28,18 +43,18 @@ router.get('/getEvent/:id', function (req, res) {
             console.log('error is thrown in get eventID');
         }
         console.log('event is: ' + event);
-        Tickets.getTicketsByEventId(event.id, function (err, tickets){
+        Tickets.getTicketsByEventId(event.id, function (err, tickets) {
             if (err) {
                 console.log('error is thrown in get tickt');
             }
-            console.log('tickets array : '+ tickets)
-            appNpay.getAppNpayByEventId(event.id, function(err, infos){
+            console.log('tickets array : ' + tickets)
+            appNpay.getAppNpayByEventId(event.id, function (err, infos) {
                 if (err) {
                     console.log('error is thorwn in get infos');
                 }
                 res.render('checkEvent', {
-                    user : req.user,
-                    event: event, 
+                    user: req.user,
+                    event: event,
                     tickets: tickets,
                     appNpay: infos
                 });
@@ -69,17 +84,41 @@ router.post('/newevent', function (req, res) {
 });
 
 
-router.post('/neweventDescription', function (req, res) {
-
+router.post('/neweventDescription', parser.single('new_pic'), function (req, res, next) {
+    console.log('whats the problem..');
     //upload image ????????????????????
+    if (req.file) {
+        const image = {};
+        console.log('Uploading File...');
+        console.log(req.file) // to see what is returned to you
+        console.log('POST///reqfile');
 
-    res.render('newYoutube', {
-        user: req.user,
-        title: req.body.title,
-        veranstalter: req.body.veranstalter,
-        pic : req.body.pic,
-        beschreibung: req.body.beschreibung
-    });
+        image.url = req.file.url;
+        image.id = req.file.public_id;
+
+        console.log('übersprungen..')
+        res.render('newYoutube', {
+            user: req.user,
+            title: req.body.title,
+            veranstalter: req.body.veranstalter,
+            picUrl: image.url,
+            picId: image.id,
+            beschreibung: req.body.beschreibung
+        });
+    } else {
+        var imageUrl = 'null';
+        var imageID = 'null';
+
+        res.render('newYoutube', {
+            user: req.user,
+            title: req.body.title,
+            veranstalter: req.body.veranstalter,
+            picUrl: imageUrl,
+            picId: imageID,
+            beschreibung: req.body.beschreibung
+        });
+    }
+
 });
 
 
@@ -89,43 +128,53 @@ router.post('/neweventYoutube', function (req, res) {
         user: req.user,
         title: req.body.title,
         veranstalter: req.body.veranstalter,
-        pic : req.body.pic,
+        picUrl: req.body.picUrl,
+        picId: req.body.picId,
         beschreibung: req.body.beschreibung,
         youtube: req.body.youtube
     });
 });
 
 router.post('/neweventLokation', function (req, res) {
+    console.log(req.body.pic);
 
-    var newEvent = new Event({
-        title: req.body.title,
-        veranstalter: req.body.veranstalter,
-        pic: req.body.pic,
-        youtube: req.body.youtube,
-        lokation: req.body.lokation,
-        address: req.body.address,
-        plz: req.body.plz,
-        userId: req.user.id
-    });
+    if (req.body.picUrl) {
+        console.log(req.body.picUrl);
 
-    Event.createEvent(newEvent, function (err, event) {
-        console.log('create event');
-        if (err) {
-            req.flash('error', 'es ist etwas schiefgelaufen, warten Sie einen Moment und probieren Sie es erneut');
-            res.location('/dashboard/newtickets');
-            res.redirect('/dashboard/newtickets');
-        } else {
-            console.log('event created');
-            req.flash('success', 'Erfolgreich eröffnet');
-            // events ist gespeichert machen wir mit den tickets weiter
-            res.render('newtickets', {
-                user: req.user,
-                title: req.body.title,
-                veranstalter: req.body.veranstalter,
-                eventId: event.id
-            });
-        }
-    });
+    } else {
+        console.log('no pic.url found....___')
+    }
+
+        var newEvent = new Event({
+            title: req.body.title,
+            veranstalter: req.body.veranstalter,
+            picUrl: req.body.picUrl,
+            picId: req.body.picId,
+            youtube: req.body.youtube,
+            lokation: req.body.lokation,
+            address: req.body.address,
+            plz: req.body.plz,
+            userId: req.user.id
+        });
+    
+        Event.createEvent(newEvent, function (err, event) {
+            console.log('create event');
+            if (err) {
+                req.flash('error', 'es ist etwas schiefgelaufen, warten Sie einen Moment und probieren Sie es erneut');
+                res.location('/dashboard/newtickets');
+                res.redirect('/dashboard/newtickets');
+            } else {
+                console.log('event created');
+                req.flash('success', 'Erfolgreich eröffnet');
+                // events ist gespeichert machen wir mit den tickets weiter
+                res.render('newtickets', {
+                    user: req.user,
+                    title: req.body.title,
+                    veranstalter: req.body.veranstalter,
+                    eventId: event.id
+                });
+            }
+        });
 });
 
 //the seller would like to finish the eventopening
@@ -145,7 +194,7 @@ router.post('/saveticket', function (req, res) {
     console.log(req.body);
     console.log('/////')
     var newTicket = new Ticket({
-  
+
         kategorie: kategorie,
         gueltig_datum: gueltig_datum,
         gueltig_time: gueltig_time,
